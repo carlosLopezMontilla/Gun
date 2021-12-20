9df90f6a1bcf35af59666dd49caf59fecae7ea2f
Monty014 <77298181+carlosLopezMontilla@users.noreply.github.com>
2021-12-20 16:41:37 +0100

    Sprite Pistola
    Se ha añadido el sprite de la pistola

--- Assets/_Scripts/MouseMove.cs
+++ Assets/_Scripts/MouseMove.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class MouseMove : MonoBehaviour
 {
     public GameObject mira;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
